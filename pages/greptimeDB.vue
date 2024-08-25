c6b7ef5ab8514a3e02314edcde35964649f3496a
yu <2665764951@qq.com>
2024-08-25 10:40:44 +0800

    rename page and components of item2

---
+++ pages/greptimeDB.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import ScalingHorizontally from "~/components/svg/greptimeDB/ScalingHorizontally.vue";
+import UnifiedAnalysis from "~/components/svg/greptimeDB/UnifiedAnalysis.vue";
+import CostEffective from "~/components/svg/greptimeDB/CostEffective.vue";
+import Compatibility from "~/components/svg/greptimeDB/Compatibility.vue";
+
+const { t } = useI18n();
+</script>
+
+<template>
+  <div class="lg:tw-hidden">
+    <div class="tw-px-4 tw-py-[3.75rem]">
+      <p class="tw-text-center tw-text-3xl tw-font-black">GreptimeDB</p>
+      <p class="tw-mb-16 tw-text-center tw-text-lg">
+        {{ t("GreptimeDB.Subheader") }}
+      </p>
+      <div class="tw-mb-8 tw-flex tw-w-full tw-justify-between tw-space-x-5">
+        <greptimeDB-card
+          :svg="ScalingHorizontally"
+          :title="t('GreptimeDB.ScalingHorizontally.title')"
+          >{{ t("GreptimeDB.ScalingHorizontally.message") }}
+        </greptimeDB-card>
+        <greptimeDB-card
+          :svg="UnifiedAnalysis"
+          :title="t('GreptimeDB.UnifiedAnalysis.title')"
+          >{{ t("GreptimeDB.UnifiedAnalysis.message") }}
+        </greptimeDB-card>
+      </div>
+      <div class="tw-mb-8 tw-flex tw-w-full tw-justify-between tw-space-x-5">
+        <greptimeDB-card
+          :svg="CostEffective"
+          :title="t('GreptimeDB.Cost-effective.title')"
+          >{{ t("GreptimeDB.Cost-effective.message") }}
+        </greptimeDB-card>
+        <greptimeDB-card
+          :svg="Compatibility"
+          :title="t('GreptimeDB.Compatibility.title')"
+          >{{ t("GreptimeDB.Compatibility.message") }}
+        </greptimeDB-card>
+      </div>
+      <div class="tw-flex tw-justify-center tw-space-x-6">
+        <v-btn color="white" density="default" class="hover:tw-scale-105"
+          >GreptimePlay
+        </v-btn>
+        <v-btn variant="outlined" density="default" class="hover:tw-scale-105"
+          >{{ t("GreptimeDB.Button") }}
+        </v-btn>
+      </div>
+    </div>
+  </div>
+  <div class="tw-hidden tw-w-full tw-py-[6.25rem] lg:tw-block">
+    <p class="tw-text-center tw-text-3xl tw-font-black">GreptimeDB</p>
+    <p class="tw-mb-16 tw-text-center tw-text-lg">
+      {{ t("GreptimeDB.Subheader") }}
+    </p>
+    <div class="tw-mb-8 tw-flex tw-w-full tw-justify-between tw-space-x-5">
+      <greptimeDB-card
+        :svg="ScalingHorizontally"
+        :title="t('GreptimeDB.ScalingHorizontally.title')"
+        >{{ t("GreptimeDB.ScalingHorizontally.message") }}
+      </greptimeDB-card>
+      <greptimeDB-card
+        :svg="UnifiedAnalysis"
+        :title="t('GreptimeDB.UnifiedAnalysis.title')"
+        >{{ t("GreptimeDB.UnifiedAnalysis.message") }}
+      </greptimeDB-card>
+      <greptimeDB-card
+        :svg="CostEffective"
+        :title="t('GreptimeDB.Cost-effective.title')"
+        >{{ t("GreptimeDB.Cost-effective.message") }}
+      </greptimeDB-card>
+      <greptimeDB-card
+        :svg="Compatibility"
+        :title="t('GreptimeDB.Compatibility.title')"
+        >{{ t("GreptimeDB.Compatibility.message") }}
+      </greptimeDB-card>
+    </div>
+    <div class="tw-flex tw-justify-center tw-space-x-6">
+      <v-btn color="white" density="default" class="hover:tw-scale-105"
+        >GreptimePlay
+      </v-btn>
+
+      <v-btn variant="outlined" density="default" class="hover:tw-scale-105"
+        >{{ t("GreptimeDB.Button") }}
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.v-btn {
+  text-transform: none !important;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import Home from "~/pages/home.vue";
 import Optimized from "~/pages/optimized.vue";
-import Item2 from "~/pages/item2.vue";
+import GreptimeDB from "~/pages/greptimeDB.vue";
 import Item3 from "~/pages/item3.vue";
 import Item4 from "~/pages/item4.vue";
 import Item5 from "~/pages/item5.vue";
@@ -16,7 +16,7 @@ definePageMeta({
   <div class="tw-mx-auto 2xl:tw-w-[82.5rem]">
     <home></home>
     <optimized></optimized>
-    <item2></item2>
+    <greptimeDB></greptimeDB>
     <item3></item3>
     <item4></item4>
     <item5></item5>
